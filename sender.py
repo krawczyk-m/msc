67eddb32b8900d19d0158799ccdf23b679a9cd4d
Michał Krawczyk <krawczyk.michal91@gmail.com>
2016-04-23 16:20:29 +0200

    [SENDER] Added sender.py which is a simple script to test configuration. The script prints incoming packages and accepts them

---
+++ sender.py
@@ -0,0 +1,18 @@
+from netfilterqueue import NetfilterQueue
+from scapy.layers.inet import IP
+import pprint
+
+
+def print_and_accept(pkt):
+    ip = IP(pkt.get_payload())
+    ip.show()
+    pkt.set_payload(str(ip))
+    pkt.accept()
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(1, print_and_accept)
+
+try:
+    nfqueue.run()
+except KeyboardInterrupt:
+    print
