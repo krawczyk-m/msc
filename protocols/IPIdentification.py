9a4df9b169f97727db25a64cd911a16180fbae31
Michał Krawczyk <krawczyk.michal91@gmail.com>
2016-04-23 21:46:39 +0200

    [CODE] Implement IPIdentification protocol along with tests

---
+++ protocols/IPIdentification.py
@@ -0,0 +1,28 @@
+from protocols.IPProtocol import IPProtocol
+from protocols.util.BitConverter import BitConverter
+from protocols.error.LayerNotFound import LayerNotFound
+
+
+class IPIdentification(IPProtocol):
+    """
+    Modifies the Identification field of the IP protocol
+    """
+    field = "id"
+    max_bits = 16
+
+    @classmethod
+    def set(cls, packet, bit_array):
+        if len(bit_array) > cls.max_bits:
+            raise ValueError("You can not set more bits than {}".format(cls.max_bits))
+        setattr(packet, cls.field, BitConverter.int(bit_array))
+        return packet
+
+    # TODO use bits limiting
+    @classmethod
+    def get(cls, packet, bits=0):
+        layer = packet.getlayer(cls.layer)
+        if not layer:
+            raise LayerNotFound("Layer {} not found in packet".format(cls.layer))
+        return getattr(layer, cls.field)
+
+
---
+++ protocols/tests/IPIdentificationTest.py
@@ -0,0 +1,32 @@
+import unittest
+from scapy.layers.inet import IP
+
+from protocols.IPIdentification import IPIdentification
+
+
+class IPIdentificationTest(unittest.TestCase):
+
+    def setUp(self):
+        self.packet = IP()
+        self.packet.id = 2**16 - 1  # max for the field
+
+    def test_set_zero(self):
+        modified = IPIdentification.set(self.packet, [0])
+        self.assertEqual(0, modified.id)
+
+    def test_set_one(self):
+        modified = IPIdentification.set(self.packet, [1])
+        self.assertEqual(1, modified.id)
+
+    def test_set_one_2(self):
+        modified = IPIdentification.set(self.packet, [0, 1])
+        self.assertEqual(1, modified.id)
+
+    def test_set_63(self):
+        modified = IPIdentification.set(self.packet, [0, 0, 1, 1, 1, 1, 1, 1])
+        self.assertEqual(63, modified.id)
+
+    def test_set_max_less_1(self):
+        array = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+        modified = IPIdentification.set(self.packet, array)
+        self.assertEqual(2**16 - 2, modified.id)
