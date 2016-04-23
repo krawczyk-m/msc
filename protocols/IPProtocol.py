c00e69a3c5d53bd3ab91c47ed7f7458ffd92b455
Michał Krawczyk <krawczyk.michal91@gmail.com>
2016-04-23 20:58:50 +0200

    [CODE] Add base classes for IP and TCP layers

---
+++ protocols/IPProtocol.py
@@ -0,0 +1,8 @@
+from protocols.ProtocolBase import ProtocolBase
+
+
+class IPProtocol(ProtocolBase):
+    """
+    Represents a family of protocols located in the IP layer
+    """
+    layer = "IP"
---
+++ protocols/TCPProtocol.py
@@ -0,0 +1,8 @@
+from protocols.ProtocolBase import ProtocolBase
+
+
+class TCPProtocol(ProtocolBase):
+    """
+    Represents a family of protocols located in the TCP layer
+    """
+    layer = "TCP"
