8a0b13a4006d1a71bb4d244e377c7a7ce6be26cd
Michał Krawczyk <krawczyk.michal91@gmail.com>
2016-04-24 17:51:25 +0200

    [CODE] iptables int -> str parameters. Implemented delete_rule method

--- client/iptables.py
+++ client/iptables.py
@@ -11,15 +11,16 @@ class IPTables(object):
     def set_rule(chain_name, queue_num):
         """
         Sets an NFQUEUE target for a given chain. The matching packets are forwarded to the queue specified by 'queue_num'
-        :param chain:           the name of the chain for which to set a new rule
+        :param chain_name:      the name of the chain for which to set a new rule
         :param queue_num:       the number of the NFQUEUE to set the forwarding for
+        :returns rule:          the object representing the set rule
         """
 
         rule = iptc.Rule()
         rule.protocol = "tcp"
 
         tcp_match = rule.create_match("tcp")
-        tcp_match.dport = 12345
+        tcp_match.dport = "12345"
 
         connbytes_match = rule.create_match("connbytes")
         connbytes_match.set_parameter("connbytes-mode", "packets")
@@ -32,4 +33,15 @@ class IPTables(object):
         chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), chain_name)
         chain.insert_rule(rule)
 
+        return rule
 
+    @staticmethod
+    def delete_rule(chain_name, rule):
+        """
+        Removes the rule from the given chain
+        :param chain_name:      name of the chain
+        :param rule:            object of the rule to be removed
+        """
+
+        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), chain_name)
+        chain.delete_rule(rule)
