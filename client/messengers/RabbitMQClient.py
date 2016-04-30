73575440cc3bf258be47dd565f4ce899d19d1d71
Michał Krawczyk <krawczyk.michal91@gmail.com>
2016-04-30 19:14:16 +0200

    [CODE] RabbitMQClient - added configuration load

--- client/messengers/RabbitMQClient.py
+++ client/messengers/RabbitMQClient.py
@@ -7,10 +7,12 @@ class RabbitMQClient(object):
         self.response = None
         self.config = config
 
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.config["host"]))
+        self._load_config()
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.rabbit_host))
         self.channel = self.connection.channel()
 
-        self.channel.basic_consume(self._receive, no_ack=True, queue=self.config["listen_queue"])
+        self.channel.basic_consume(self._receive, no_ack=True, queue=self.sender_full_queue_name)
         self.consume_thread = threading.Thread(target=self._consume)
         self.consume_thread.start()
 
@@ -28,10 +30,21 @@ class RabbitMQClient(object):
 
     def notify(self, message):
         self.channel.basic_publish(exchange='',
-                                   routing_key=self.config["notify_queue"],
+                                   routing_key=self.receiver_full_queue_name,
                                    body=message)
 
     def join(self):
         # self.channel.stop_consuming() # seems to not break channel.start_consuming()
         # self.consume_thread.join() # since channel.start_consuming() does not stop, this hangs forever
         pass
+
+    # TODO try to do this with RPC queues...
+    def _load_config(self):
+        self.host = self.config.get("General", "host")
+        self.peer_address = self.config.get("Steg", "peer")
+        self.rabbit_host = self.config.get("RabbitMQ", "host")
+
+        sender_queue_name = self.config.get("RabbitMQ", "sender_queue_name")
+        receiver_queue_name = self.config.get("RabbitMQ", "receiver_queue_name")
+        self.sender_full_queue_name = "{}_{}".format(self.host, sender_queue_name)
+        self.receiver_full_queue_name = "{}_{}".format(self.peer_address, receiver_queue_name)
