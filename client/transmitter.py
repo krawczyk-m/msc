51239f604660b13d642a3aa351166f5eb2e06274
Michał Krawczyk <krawczyk.michal91@gmail.com>
2016-04-30 18:27:22 +0200

    [DOC] Added documentation to Transmitter

--- client/transmitter.py
+++ client/transmitter.py
@@ -18,11 +18,22 @@ class Transmitter(object):
     # TODO 2 what if len(bit_array) greater than available bit space for all protocols?
     # TODO 3 same as 2 for a single protocol - need better handling e.g. return the bits that are left
     def embed(self, packet, bit_array):
+        """
+        Embeds the bit_array into the passed in packet
+        :param packet:      scapy.layers.inet.IP packet
+        :param bit_array:   bit array in the form of integer 0's and 1's
+        :return:            modified scapy.layers.inet.IP packet with steganographic bits
+        """
         for protocol in self.protocols:
             packet = protocol.set(packet, bit_array)
         return packet
 
     def extract(self, packet):
+        """
+        Extracts steganographic bits from the passed in packet
+        :param packet:       scapy.layers.inet.IP packet
+        :return:             bit array in the form of integer 0's and 1's
+        """
         bit_array = []
         for protocol in self.protocols:
             bit_array.extend(protocol.get(packet))
